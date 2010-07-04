92e6f3ed377626a692764d509b3f086c16d838aa
Eric Fer <eric.fer@gmail.com>
2010-07-03 21:33:51 -0300

    Adicionado script para remoção desse projeto: remove todas as referências no home da máquina

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+echo "Removing home files..."
+rm ~/.bash_profile
+rm ~/.autotest
+rm ~/.gemrc
+rm ~/.git-completion.sh
+rm ~/.gitconfig
+rm ~/.gitignore
+rm ~/.gitk
+rm ~/.irbrc
+rm ~/.caprc
+rm ~/.privaterc
+
+echo "Removing home dirs..."
+rm ~/bin
